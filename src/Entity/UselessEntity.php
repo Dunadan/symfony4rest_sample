b7e0b2f0b0f1947837d38d0a75d45d0c01412c68
sergei <sergei@creatiff.com.ua>
2018-01-17 11:49:43 +0200

    LoadBalancer

--- src/Entity/UselessEntity.php
+++ src/Entity/UselessEntity.php
@@ -5,7 +5,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity
  * @ORM\Table(name="useless_entities")
  * @ORM\Entity(repositoryClass="App\Repository\UselessRepository")
  */
